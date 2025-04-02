05f1b915923239559b5991dd36d85232efa9bc60
Pravesh532 <praveshpatel9569@gmail.com>
2025-04-02 16:09:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+ document.getElementById("ageForm").addEventListener("submit",function(event){
+        event.preventDefault();
+
+        let name = document.getElementById("name").value;
+        let age = document.getElementById("age").value;
+         
+         checkAge(age,name)
+         .then((message) =>{
+            alert(message);
+         })
+         .catch((error)=>{
+            alert(error);
+         })
+
+
+         function checkAge(age ,name){
+            return new Promise((resolve,resject)=>{
+                setTimeout(() => {
+                    if(age>10){
+                        resolve(`Welcome, ${name}. You can vote.`);
+                    }else{
+                        resject(`Oh sorry, ${name}. You aren't old enough.`);
+                    }
+                }, 1000);
+            })
+         }
+
+    })
+
+}
